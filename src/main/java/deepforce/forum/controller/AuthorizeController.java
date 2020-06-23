712e00a7243958c9a8319ee81a8717fc4e5103dd
deepforce <deepforce31@gmail.com>
2020-06-23 16:07:30 -0700

    Add SQL Database Logic

--- src/main/java/deepforce/forum/controller/AuthorizeController.java
+++ src/main/java/deepforce/forum/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package deepforce.forum.controller;
 
 import deepforce.forum.dto.AccessTokenDTO;
 import deepforce.forum.dto.GithubUser;
+import deepforce.forum.mapper.UserMapper;
+import deepforce.forum.model.User;
 import deepforce.forum.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -21,9 +26,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                            @RequestParam(name="state") String state) {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -31,8 +40,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            // Login success! Save Cookie & Session, redirect to index
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            // Login fail! Retry!
+            return "redirect:/";
+        }
     }
 }
