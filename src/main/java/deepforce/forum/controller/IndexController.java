e6240fcdd28359f143dbadd7d598812e133e315c
deepforce <deepforce31@gmail.com>
2020-06-24 17:31:01 -0700

    Add question list

--- src/main/java/deepforce/forum/controller/IndexController.java
+++ src/main/java/deepforce/forum/controller/IndexController.java
@@ -1,13 +1,17 @@
 package deepforce.forum.controller;
 
+import deepforce.forum.dto.QuestionDTO;
 import deepforce.forum.mapper.UserMapper;
 import deepforce.forum.model.User;
+import deepforce.forum.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,10 +19,14 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
+        if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
@@ -30,7 +38,8 @@ public class IndexController {
                 }
             }
         }
-
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
---
+++ src/main/java/deepforce/forum/service/QuestionService.java
@@ -0,0 +1,36 @@
+package deepforce.forum.service;
+
+import deepforce.forum.dto.QuestionDTO;
+import deepforce.forum.mapper.QuestionMapper;
+import deepforce.forum.mapper.UserMapper;
+import deepforce.forum.model.Question;
+import deepforce.forum.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<QuestionDTO> list() {
+        List<Question> questions = questionMapper.list();
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        return questionDTOList;
+    }
+}
