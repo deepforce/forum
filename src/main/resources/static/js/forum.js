5a9771c8b8b6e9225842f87ba1fe4faf62e33489
deepforce <deepforce31@gmail.com>
2020-06-27 17:52:56 -0700

    Add Reply Frontend

---
+++ src/main/resources/static/js/forum.js
@@ -0,0 +1,30 @@
+function post() {
+    let questionId = $('#question_id').val();
+    let content = $('#comment_content').val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code === 200) {
+                $('#comment_section').hide();
+            } else {
+                if (response.code === 2003) {
+                    let isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=cd1b2839576d0971f83c&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable", true);
+                    }
+                }
+                alert(response.message);
+            }
+            console.log(response);
+        },
+        dataType: "json"
+    });
+}
